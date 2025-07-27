db6294dff6ad4f1b9554daf0065cc08ea1afb412
rijomd <rijomd09@gmail.com>
2025-07-27 12:55:30 +0530

    api initialized

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,5 +1,33 @@
+import { DB } from "@/lib/db";
+import { response } from "@/lib/utils";
 import { NextResponse } from "next/server"
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
 
 export const GET = async () => {
-    return NextResponse.json({ success: true })
+    return NextResponse.json("success")
+}
+
+export const POST = async (req: Request) => {
+    const body = await req.json();
+    const { email, name, password } = body;
+
+    const isExistEmail = await DB.user.findUnique({ where: { email: email } });
+
+    if (isExistEmail) {
+        return response({ data: null, message: "user already exist", code: 400 });
+    }
+
+    const hashPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = await DB.user.create({
+        data: {
+            email: email,
+            name: name,
+            password: hashPassword
+        }
+    });
+
+    return response({ data: user.name, message: "user created successfully" });
 }
---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+type ResponseType = {
+    data: any;
+    message?: string;
+    code?: number;
+}
+
+export const response = ({ data, message, code }: ResponseType) => NextResponse.json({
+    data: data, message: message || "success", code: code || 200
+});
